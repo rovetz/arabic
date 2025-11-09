b4515b9d84068de009ff38cb67812065a896bc57
Andrey <zakbox@gmail.com>
2025-11-09 22:32:24 +0100

    Update iso233_test.rb

--- test/iso233_test.rb
+++ test/iso233_test.rb
@@ -4,11 +4,10 @@ require "test_helper"
 
 class Iso233Test < Minitest::Test
   def test_it_transliterates_iso233
-    assert_equal "alʿrbyẗ", Arabic.t("العربية", to: :iso233)
-    assert_equal "alšrq alʾdny", Arabic.t("الشرق الأدنى", to: :iso233)
+    assert_equal "ʾrdw", Arabic.t("اردو", :iso233)
   end
 
   def test_it_transliterates_numbers
-    assert_equal "0123456789", Arabic.t("٠١٢٣٤٥٦٧۸٩", to: :iso233)
+    assert_equal "0123456789", Arabic.t("٠١٢٣٤٥٦٧۸٩", :iso233)
   end
 end
